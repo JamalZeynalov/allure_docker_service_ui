35cbd1ef6d5c8215f4a33f84900c3db1823f03ee
Jamal Zeinalov <Jamal_Zeinalov@epam.com>
2020-07-26 19:33:29 +0300

    README.md is updated

--- allure_docker_service_ui/urls.py
+++ allure_docker_service_ui/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('service_ui.urls')),
-    path('admin/', admin.site.urls),
+    path('', include('service_ui.urls'))
 ]
