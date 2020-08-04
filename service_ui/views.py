f32acf0acccef527564e3c16dd690571d6489ba7
Jamal Zeinalov <Jamal_Zeinalov@epam.com>
2020-08-04 15:36:58 +0300

    no_cache for all views

--- service_ui/views.py
+++ service_ui/views.py
@@ -6,6 +6,8 @@ from service_ui.utils.bar_plots import generate_tests_history_plot
 from service_ui.utils.projects_filtering import get_latest_daily, get_all_daily_reports
 
 
+@never_cache
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 def daily(request, report_date: str):
     if len(latest_10_projects := get_latest_daily()):
         request.report_date = report_date
@@ -16,6 +18,8 @@ def daily(request, report_date: str):
         return render(request, 'service_ui/not_found.html')
 
 
+@never_cache
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 def home(request):
     if len(latest_10_projects := get_latest_daily()):
         report_date = str(latest_10_projects[-1])
@@ -24,6 +28,8 @@ def home(request):
         return render(request, 'service_ui/not_found.html')
 
 
+@never_cache
+@cache_control(max_age=0, no_cache=True, no_store=True, must_revalidate=True)
 def all_reports(request):
     daily_reports = get_all_daily_reports()
     return render(request, 'service_ui/all_reports.html', {'projects': daily_reports})
